079d544738f385a952fb84f03c1f92a8b7382c73
Martin Abbott <abbott01@googlemail.com>
2023-08-03 17:44:13 +0100

    Created initial scene, geometry, lighting and camera

---
+++ test-page/main.js
@@ -0,0 +1,25 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+
+const material = new THREE.MeshStandardMaterial({
+	color: "#00ff83",
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 15, 10);
+scene.add(light);
+
+const camera = new THREE.PerspectiveCamera(45, 800 / 600);
+camera.position.z = 20;
+scene.add(camera);
+
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGL1Renderer({ canvas });
+renderer.setSize(800, 600);
+renderer.render(scene, camera);
